018cf3eb928497ede9c20d1af61fc87a91e5cb8e
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-12-05 07:32:32 -0500

    initial commit; this one works in VS Code but not on CodeWars

---
+++ Div_by_7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Div_by_7
+{
+    class Program
+    {
+        public static long count = 0;
+
+        public static long[] Seven(long m)
+        {
+            // your code
+            long[] numbersDivisibleBySeven = { 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98 };
+
+            if (m <= 98)
+            {
+                if (numbersDivisibleBySeven.Contains(m))
+                {
+                    return new long[] { m, count };
+                }
+                else
+                {
+                    return new long[] { 0, 0 };
+                }
+            }
+            else
+            {
+                long lastDigit = m % 10;
+                long tenX = m - lastDigit;
+                long x = tenX / 10;
+                long intermediateResult = x - 2 * lastDigit;
+                count++;
+                return Seven(intermediateResult);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            long[] result = Seven(371);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
