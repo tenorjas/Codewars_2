40a933fb2b06d9eeb76f3ff589ad78815e0ae1bf
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-12-12 12:51:31 -0500

    solved array_diff

---
+++ array_diff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace array_diff
+{
+    class Program
+    {
+        public class Kata
+        {
+            public static int[] ArrayDiff(int[] a, int[] b)
+            {
+                // Your brilliant solution goes here
+                // It's possible to pass random tests in about a second ;)
+                List<int> finalList = new List<int>();
+
+                for (var i = 0; i < a.Length; i++)
+                {
+                    if (!b.Contains(a[i]))
+                    {
+                        finalList.Add(a[i]);
+                    }
+                }
+
+                int[] finalArray = new int[finalList.Count];
+                finalArray = finalList.ToArray();
+                return finalArray;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
