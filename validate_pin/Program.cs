80dc784caa510d4410c7ca32749b35baca74f51f
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-12-12 12:11:06 -0500

    solved validate pin kata

---
+++ validate_pin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace validate_pin
+{
+    class Program
+    {
+        public class Kata
+        {
+            public static bool ValidatePin(string pin)
+            {
+                bool validLength = false;
+                bool allDigits = pin.All(Char.IsDigit);
+
+                if (pin.Length == 4 || pin.Length == 6)
+                {
+                    validLength = true;
+                }
+
+                return validLength && allDigits;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
