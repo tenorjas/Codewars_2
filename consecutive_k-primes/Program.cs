78404f36d2bcef4921740af466fa2978f42a70f2
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-01-21 19:06:27 -0500

    consecutive k-primes still incomplete, getting compilation errors

--- consecutive_k-primes/Program.cs
+++ consecutive_k-primes/Program.cs
@@ -9,7 +9,8 @@ namespace consecutive_k_primes
             // your code
             int numberOfConsecutives = 0;
 
-            int numberOfPrimes(long number)
+            // this is an internal function that calculates the number of prime factors of a number
+            int NumberOfPrimes(long number)
             {
                 int numPrimes = 0;
                 int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 };
@@ -20,16 +21,27 @@ namespace consecutive_k_primes
                     number = number / primes[i];
                     numPrimes++;
                     i = 0; // if we've found a prime factor, start over at 2
-                    var temp = numberOfPrimes(number);
+                    var temp = NumberOfPrimes(number);
                 }
                 else
                 {
                     i++; // move on to the next prime number
-                    var temp = numberOfPrimes(number);
+                    var temp = NumberOfPrimes(number);
                 }
                 return numPrimes;
             }
-            // some other stuff that I haven't figured out yet goes here
+
+            for (var j = 0; j < arr.Length - 1; j++)
+            {
+                int a = NumberOfPrimes(arr[j]);
+                int b = NumberOfPrimes(arr[j + 1]);
+
+                if (a == k && a == b)
+                {
+                    numberOfConsecutives++;
+                }
+            }
+
             return numberOfConsecutives;
         }
         static void Main(string[] args)
