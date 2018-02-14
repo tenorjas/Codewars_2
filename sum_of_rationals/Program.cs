f4ceea930e3fc78027ea8fe13336626403cdc072
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-02-14 16:48:06 -0500

    trying to figure out array of arrays

--- sum_of_rationals/Program.cs
+++ sum_of_rationals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sum_of_rationals
 {
@@ -7,11 +8,18 @@ namespace sum_of_rationals
         public static string SumFracts(int[,] l)
         {
             // your code
+            //decimal sum = 0;
+
+            Console.WriteLine(l.Length);
+
+            return "zero";
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[,] m1 = new int[,] { {1,2}, {2,9}, {3,18}, {4,24}, {6,48} };
+            SumFracts(m1);
         }
     }
 }
