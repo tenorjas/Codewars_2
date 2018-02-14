a20eadda8ace716c2888f623fcbefe0ac2293779
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-02-14 15:48:25 -0500

    initial commit of sum of rationals

---
+++ sum_of_rationals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sum_of_rationals
+{
+    class Program
+    {
+        public static string SumFracts(int[,] l)
+        {
+            // your code
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
