8f4d8ebafd3259876ecfe7e26d842d5420140213
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-02-15 12:10:32 -0500

    initial commit of alphabet war

---
+++ alphabet_war/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alphabet_war
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
