0e85e9dc4ee064dad2c0f83091eb76f274954dcc
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-02-26 14:53:59 -0500

    completed Dist part of braking well kata

---
+++ braking_well/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace braking_well
+{
+    class Program
+    {
+        public static double Dist(double v, double mu)        // suppose reaction time is 1
+        {
+            // your code
+            double g = 9.81;
+            double t = 1;
+            double v1 = v * 0.27777777777777777778;
+
+            double reactionDistance = v1 * t;
+            double brakingDistance = v1 * v1 / (2 * mu * g);
+
+            // Console.WriteLine(reactionDistance);
+            // Console.WriteLine(brakingDistance);
+
+            return reactionDistance + brakingDistance;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double d1 = Dist(100,0.7);
+            Console.WriteLine(d1);
+        }
+    }
+}
