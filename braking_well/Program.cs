86d2518d292dd04238a2f41363739591b4c7d1d7
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-02-26 16:07:35 -0500

    have now solved both the distance and speed calculations

--- braking_well/Program.cs
+++ braking_well/Program.cs
@@ -20,12 +20,34 @@ namespace braking_well
             return reactionDistance + brakingDistance;
         }
 
+        public static double Speed(double d, double mu)       // suppose reaction time is 1
+        {
+            // your code
+            double g = 9.81;
+            double t = 1;
+
+            // now we have to do some algebra
+            // namely find the positive root of a quadratic equation
+            double determinant = t * t + 2 * d / (mu * g);
+            double sqrtD = Math.Sqrt(determinant);
+
+            // root of quadratic equation is initial velocity in mps
+            double v1 = mu * g *(-1*t + sqrtD);
+            // now convert to kph
+            double v = v1 / 0.27777777777777777778;
+
+            return v;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            double d1 = Dist(100,0.7);
+            double d1 = Dist(100, 0.7);
             Console.WriteLine(d1);
+
+            double v1 = Speed(83.9598760937531, 0.7);
+            Console.WriteLine(v1);
         }
     }
 }
