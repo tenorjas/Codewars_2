57d2cd44b572e2a818ce3eceefde10275c9c4134
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2018-02-26 16:38:59 -0500

    initial commit of consecutive strings

---
+++ consecutive_strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace consecutive_strings
+{
+    class Program
+    {
+        public static String LongestConsec(string[] strarr, int k)
+        {
+            // your code
+            
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
